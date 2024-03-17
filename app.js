014b255f65a7d7cebba424f1d13136c7eb6c9843
svetlana-tyshkevich <panfilova.ss@gmail.com>
2024-03-17 16:58:54 +0200

    add common scene action

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-import { stdin as input, stdout as output } from 'node:process';
-import * as readline from 'node:readline/promises';
+import { scenario } from './src/scenario.js';
 
-const rl = readline.createInterface({ input, output });
+let currentLevel = scenario;
 
-rl.on('line', async (line) => {
-    try {
-        console.log(line);
-    } catch {
-        console.log('Что-то пошло не так. Игра окончена!');
-        rl.close();
+const setScene = () => {
+    if (currentLevel.title) console.log(currentLevel.title);
+    if (currentLevel.description) console.log(currentLevel.description);
+    if (currentLevel.question && currentLevel.variants.length) {
+        inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'choice',
+                    message: currentLevel.question,
+                    choices: [{
+                        name: currentLevel.variants[0].answer,
+                        value: 0,
+                    }, {
+                        name: currentLevel.variants[1].answer,
+                        value: 1,
+                    }],
+
+                },
+            ]).then((answer) => {
+            currentLevel = currentLevel.variants[answer.choice];
+            console.log('\n');
+            setScene();
+        })
     }
-});
+};
 
-rl.on('SIGINT', () => {
-    rl.close();
-});
+setScene();
